59512f226647c79c1f42baea2a7b7cd71ab13c5b
Rylan Cox <rylanrcox@gmail.com>
2023-04-09 19:27:56 -0700

    Fully implement likePost.php

--- Webpages/PHP/likePost.php
+++ Webpages/PHP/likePost.php
@@ -25,19 +25,45 @@
             
             include('credentials.php');
 
-            $mysqli = new mysqli($servername, $username, $password, $dbname);
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
             
-            if($postLike){
-                $stmt = $mysqli->prepare("UPDATE posts SET votes = votes + 1 WHERE postID = ?");
-                $stmt->bind_param('i', $postID);
-            }else if($commentLike){
-                $stmt = $mysqli->prepare("UPDATE comments SET votes = votes + 1 WHERE commentID = ?");
-                $stmt->bind_param('i', $commentID);
+            $error = mysqli_connect_error();
+            if($error != null)
+            {
+                $output = 'Unable to connect to databse!';
+                exit($output);
+            } else {
+                if($postLike){
+                    $sql = "UPDATE posts SET votes = votes + 1 WHERE postID = ?";
+                    $stmt = mysqli_prepare($conn, $sql);
+                    mysqli_stmt_bind_param($stmt, 'i', $postID);
+                }else if($commentLike){
+                    $sql = "UPDATE comments SET votes = votes + 1 WHERE commentID = ?";
+                    $stmt = mysqli_prepare($conn, $sql);
+                    mysqli_stmt_bind_param($stmt, 'i', $commentID);
+                }
             }
 
-			$stmt->execute();
+            mysqli_stmt_execute($stmt);
             $stmt->close();
             
+            if($postLike){
+                $sql = "INSERT INTO voted VALUES(default, ?, null, ?)";
+                
+                $stmt = mysqli_prepare($conn, $sql);
+                mysqli_stmt_bind_param($stmt, 'ii', $postID, $userID);
+                
+            }else if($commentLike){
+                $sql = "INSERT INTO voted VALUES(default, null, ?, ?)";
+                $stmt = mysqli_prepare($conn, $sql);
+                mysqli_stmt_bind_param($stmt, 'ii', $commentID, $userID);
+            }
+            $result = mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
+
+            echo $result;
+
+            $stmt->close();
+            mysqli_close($conn);
 
         }catch(mysqli_sql_exception $e){
             echo json_encode($e);
